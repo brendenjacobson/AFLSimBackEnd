d94bde11998a91f928785b929f89b018b6feae39
brendenjacobson <brendenjacobson@gmail.com>
2020-06-04 09:12:01 +0930

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('Team'); // Create a table for 'Team' with default settings
+mobileApp.tables.add('Season'); // Create a table for 'Season' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
