35c4063a7f7874f2c57a75981d08e30459eb7dd1
brendenjacobson <brendenjacobson@gmail.com>
2020-06-05 10:34:49 +0930

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 
 mobileApp.tables.add('Team'); // Create a table for 'Team' with default settings
 mobileApp.tables.add('Season'); // Create a table for 'Season' with default settings
+mobileApp.tables.add('Round'); // Create a table for 'Round' with default settings
+mobileApp.tables.add('Match'); // Create a table for 'Match' with default settings
+mobileApp.tables.add('Result'); // Create a table for 'Result' with default settings
+mobileApp.tables.add('Ground'); // Create a table for 'Ground' with default settings
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
