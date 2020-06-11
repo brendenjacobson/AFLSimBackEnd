843977848f13b14a02218ea2a2c31491a37d759b
Brenden <brenden.jacobson@outlook.com>
2020-06-11 17:12:12 +0930

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ mobileApp.tables.add('Season'); // Create a table for 'Season' with default sett
 mobileApp.tables.add('Round'); // Create a table for 'Round' with default settings
 mobileApp.tables.add('Match'); // Create a table for 'Match' with default settings
 mobileApp.tables.add('Result'); // Create a table for 'Result' with default settings
-mobileApp.tables.add('Ground'); // Create a table for 'Ground' with default settings
+mobileApp.tables.add('Ground'); // Create a table for 'Ground' with default settings 
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
